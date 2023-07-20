5a39bb9ff37f842dfce4d22ebb384cb726320b19
Martin Donadieu <martindonadieu@gmail.com>
2023-07-20 18:57:07 +0100

    fix: use same methods as fingerprint

--- ios/Plugin/CapacitorFingerprintPlugin.swift
+++ ios/Plugin/CapacitorFingerprintPlugin.swift
@@ -7,43 +7,89 @@ import FingerprintPro
  */
 @objc(CapacitorFingerprintPlugin)
 public class CapacitorFingerprintPlugin: CAPPlugin {
-    let client
+    private var fpjsClient: FingerprintClientProviding?
 
     @objc func load(_ call: CAPPluginCall) {
         let apiKey = call.getString("apiKey", "")
-        client = FingerprintProFactory.getInstance(apiKey)
+        fpjsClient = FingerprintProFactory.getInstance(apiKey)
         call.resolve()
     }
 
-    @objc func getVisitorId(_ call: CAPPluginCall) {
+    @objc func getVisitorId(_ call: CAPPluginCall) async {
         let tags = call.getArray("tags")
         let linkedId = call.getString("linkedId")
-        do {
-            let visitorId = try await client.getVisitorId()
-            print(visitorId)
-            call.resolve([
-                "visitorId": visitorId
-            ])
-        } catch {
-            // process error
-            call.reject("Error" + error.localizedDescription)
+        guard let fpjsClient = fpjsClient else {
+            call.reject("Error: client is not initialized")
+            return
+        }
+        let metadata = CapacitorFingerprintPlugin.prepareMetadata(linkedId, tags: tags)
+        fpjsClient.getVisitorId(metadata) { result in
+            switch result {
+            case let .failure(error):
+                let description = error.description
+                call.reject("Error: ", description, error)
+            case let .success(visitorId):
+                // Prevent fraud cases in your apps with a unique
+                // sticky and reliable ID provided by FingerprintJS Pro.
+                call.resolve(["visitorId": visitorId])
+            }
         }
     }
     @objc func getVisitorData(_ call: CAPPluginCall) {
         let tags = call.getArray("tags")
         let linkedId = call.getString("linkedId")
-        do {
-            let visitorData = try await client.getVisitorData()
-            print(visitorData)
-            call.resolve([
-                "visitorData": visitorData
-            ])
-        } catch {
-            // process error
-            call.reject("Error" + error.localizedDescription)
+        guard let fpjsClient = fpjsClient else {
+            call.reject("Error: client is not initialized")
+            return
         }
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+        let metadata = CapacitorFingerprintPlugin.prepareMetadata(linkedId, tags: tags)
+        fpjsClient.getVisitorIdResponse(metadata) { result in
+            switch result {
+            case let .failure(error):
+                let description = error.description
+                call.reject("Error: ", description, error)
+            case let .success(visitorData):
+                // Prevent fraud cases in your apps with a unique
+                // sticky and reliable ID provided by FingerprintJS Pro.
+                call.resolve(["visitorData": visitorData])
+            }
+        }
+    }
+    private static func parseRegion(_ passedRegion: String?, endpoint: String?) -> Region {
+        var region: Region
+        switch passedRegion {
+        case "eu":
+            region = .eu
+        case "ap":
+            region = .ap
+        default:
+            region = .global
+        }
+
+        if let endpointString = endpoint {
+            region = .custom(domain: endpointString)
+        }
+
+        return region
+    }
+
+    private static func prepareMetadata(_ linkedId: String?, tags: JSArray?) -> Metadata {
+        var metadata = Metadata(linkedId: linkedId)
+        guard
+            let tags = tags,
+            let jsonTags = JSONTypeConvertor.convertObjectToJSONTypeConvertible(tags)
+        else {
+            return metadata
+        }
+
+        if let dict = jsonTags as? [String: JSONTypeConvertible] {
+            dict.forEach { key, jsonType in
+                metadata.setTag(jsonType, forKey: key)
+            }
+        } else {
+            metadata.setTag(jsonTags, forKey: "tag")
+        }
+
+        return metadata
     }
 }
